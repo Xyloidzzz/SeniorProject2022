1a856b4f25e2bf9416c2db35f8762c18da870b14
[hun9357] <[hun935701@gmail.com]>
2022-02-21 15:28:49 -0600

    basic design

---
+++ pages/classlist.js
@@ -0,0 +1,49 @@
+import HeadInfo from '../components/Headinfo'
+// import styles from '../styles/Home.module.css'
+import list from '../styles/Classrooms.module.css'
+import Link from 'next/link'
+
+const classList = () =>{
+    return (
+        <>
+        <div>
+            <HeadInfo title="class rooms"></HeadInfo>
+            
+            <h1 className={list.head}>My class lists</h1>
+            <button>Edit</button>
+            <button>Add Class</button>
+            <main className={list.main}>
+                <ul>
+                    <li>
+                        <h2>Class Name1</h2>
+                        <Link href='/classroom'>                     
+                        <button>Enter</button>
+                        </Link>   
+                    </li>
+                    {/* <li>
+                        <h2>Class Name2</h2>                        
+                        <button>Enter</button>
+                    </li>
+                    <li>
+                        <h2>Class Name3</h2>
+                        <button>Enter</button>
+                    </li>
+                    <li>
+                        <h2>Class Name4</h2>                        
+                        <button>Enter</button>
+                    </li> */}
+                </ul>
+            
+            </main>
+
+
+
+
+
+        </div>
+        
+        </>
+    )
+}
+
+export default classList
---
+++ pages/classroom.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Classrooms.module.css'
+import HeadInfo from '../components/Headinfo.js'
+import Link from 'next/link'
+import SideMenu from '../components/sidemenu'
+
+export default function classroom({column,rows}){
+    return(
+        <div>
+            <HeadInfo title="classroom name"></HeadInfo>
+            <SideMenu option1="edit table" route1="table"></SideMenu>
+            
+            
+        </div>
+    )
+    classroom.defaultProps = {
+        column:0,
+        rows:0
+    }
+}
