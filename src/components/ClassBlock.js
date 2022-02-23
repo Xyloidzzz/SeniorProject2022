efaa2617bc1647959adce00a6f49ba52b18f5d27
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2022-02-22 22:56:51 -0600

    testing a new SideBar component

--- src/components/ClassBlock.js
+++ src/components/ClassBlock.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, Link, IconButton } from '@chakra-ui/react'
+import { Box, Heading, Text, Link, IconButton, Divider } from '@chakra-ui/react'
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import NextLink from 'next/link'
 
@@ -6,11 +6,13 @@ const ClassBlock = ({ title, description, link, ...rest }) => {
   return (
     <Box p={5} shadow='md' borderWidth='1px' {...rest}>
       <Heading fontSize='xl'>{title}</Heading>
+      <Divider orientation='horizontal' colorScheme='black' />
       <Text mt={4}>{description}</Text>
       <NextLink href={link} passHref>
         <Link>
           <IconButton
             aria-label='Enter Class'
+            size='sm'
             icon={<ArrowForwardIcon />}
             mt={4}
             colorScheme='blue'
--- src/pages/classlist.js
+++ src/pages/classlist.js
@@ -1,44 +1,43 @@
+import { Box, Button, Container, Flex, Heading, Stack } from '@chakra-ui/react'
 import HeadInfo from '@/components/HeadInfo'
-import list from '@/styles/Classrooms.module.css'
-import Link from 'next/link'
+import ClassBlock from '@/components/ClassBlock'
+import SideBar from '@/components/SideBar'
+import styles from '@/styles/Classrooms.module.css'
 
 const classList = () => {
   return (
-    <>
-      <div>
-        <HeadInfo
-          title='Classrooms'
-          keyword='classrooms'
-          description='classroom list'
-        />
-
-        <h1 className={list.head}>My class lists</h1>
-        <button>Edit</button>
-        <button>Add Class</button>
-        <main className={list.main}>
-          <ul>
-            <li>
-              <h2>Class Name1</h2>
-              <Link href='/classroom' passHref>
-                <button>Enter</button>
-              </Link>
-            </li>
-            {/* <li>
-                    <h2>Class Name2</h2>                        
-                    <button>Enter</button>
-                </li>
-                <li>
-                    <h2>Class Name3</h2>
-                    <button>Enter</button>
-                </li>
-                <li>
-                    <h2>Class Name4</h2>                        
-                    <button>Enter</button>
-                </li> */}
-          </ul>
-        </main>
-      </div>
-    </>
+    <Container m='0' p='0'>
+      <HeadInfo
+        title='Classrooms'
+        keyword='classrooms'
+        description='classroom list'
+      />
+      <Flex w='100%'>
+        <SideBar />
+        {/* <Box>
+          <Heading fontSize='xl'>Classrooms</Heading>
+          <Button>Edit</Button>
+          <Button>Add Class</Button>
+        </Box> */}
+        <Flex
+          pos='absolute'
+          top='50%'
+          left='50%'
+          transform='translate(-50%, -50%)'
+        >
+          {/* TODO: Fix this box. Padding and Margins are wack. */}
+          <Box className={styles.main}>
+            <Stack>
+              <ClassBlock
+                title='Classroom 1'
+                description='This is a classroom description.'
+                link='/classroom'
+              ></ClassBlock>
+            </Stack>
+          </Box>
+        </Flex>
+      </Flex>
+    </Container>
   )
 }
 
