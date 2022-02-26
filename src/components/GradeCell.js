749f1f4c2940ce7e7915c2d03b550874847bd723
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2022-02-26 02:49:13 -0600

    Hotfix grade bounds only 0-9 digits

--- src/components/GradeCell.js
+++ src/components/GradeCell.js
@@ -3,31 +3,86 @@ import {
   Editable,
   EditableInput,
   EditablePreview,
+  useToast,
   Text,
+  Alert,
+  AlertIcon,
 } from '@chakra-ui/react'
 import { useState } from 'react'
+
+// TODO: need an async funciton here once DB is up to set up connection
+// however maybe this will happen better in upper level component
+// cause then we might run DB connect a million times
 
 const isValidGrade = (grade) => {
   return grade >= 0 && /[0-9]/.test(grade) ? true : false
 }
 
-async function valueListener() {
-  // here we will check the validity of the value
-}
-
 function updateDB() {
-  // here we will sumbit new cell value to DB
+  // here we will sumbit new cell value to DB -> maybe upper component not here
 }
 
-//TODO: WHEN DB IS SET THEN WE CAN FIX THE NEGATIVE NUMBERS HERE FUCK
 const GradeCell = ({ defaultValue, studentName, assignment }) => {
-  const [value, setValue] = useState(100)
+  const [value, setValue] = useState(defaultValue)
+  const [valid, setValid] = useState(true)
+
+  const submitErrorToast = useToast()
+
+  const EditableInputError = () => {
+    if (!valid) {
+      return (
+        // <Alert status='error' size='sm'>
+        //   <AlertIcon />A grade cannot be negative or non-numerical!
+        // </Alert>
+        <Text color='red.500' p='2'>
+          A grade cannot be negative, non-numerical, or empty!
+        </Text>
+      )
+    } else {
+      return null
+    }
+  }
+
+  const onChangeValueListener = (event) => {
+    isValidGrade(event.target.value) ? setValid(true) : setValid(false)
+    setValue(event.target.value)
+  }
+
+  const onCancelValueListener = (prevValue) => {
+    isValidGrade(prevValue) ? setValid(true) : setValid(false)
+  }
+
+  const onSubmitListener = (nextValue) => {
+    if (valid) {
+      // update DB
+    } else {
+      // cancel sumbit or go back to edit
+      setValue(0)
+      setValid(true)
+      submitErrorToast({
+        title: 'Invalid Text in Table Cell',
+        description:
+          'You have input a negative number or non-numerical symbol. We have reset the value to 0. Please try again.',
+        status: 'error',
+        duration: 9000,
+        position: 'top',
+        isClosable: true,
+      })
+      // refresh component if possible
+    }
+  }
+
   return (
     <Td isNumeric>
-      <Editable defaultValue={defaultValue} onSubmit={updateDB}>
+      <Editable
+        value={value}
+        onSubmit={onSubmitListener}
+        onCancel={onCancelValueListener}
+      >
         <EditablePreview />
-        <EditableInput onChange={valueListener} />
+        <EditableInput onChange={onChangeValueListener} />
       </Editable>
+      <EditableInputError />
     </Td>
   )
   GradeCell.defaultProps = {
