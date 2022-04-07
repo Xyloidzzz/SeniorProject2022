c98d48f3d2f1a13aeb4039603e36adcf9a8ea798
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2022-04-06 21:54:53 -0500

    Update getAllGrades.js
    now featuring cleaned JSON POGGY WOGGY

--- src/pages/api/class/[classID]/getAllGrades.js
+++ src/pages/api/class/[classID]/getAllGrades.js
@@ -5,7 +5,7 @@ import {
 const prisma = new PrismaClient()
 
 // get all grades based on classID from prisma database
-export const getAllGrades = async (req, res) => {
+export default async function handler(req, res) {
   const {
     classID
   } = req.query
@@ -19,9 +19,7 @@ export const getAllGrades = async (req, res) => {
         assignments: true
       }
     })
-    const students = findClass.students
-    const assignments = findClass.assignments
-    const getData = await Promise.all(students.map(async (student) => {
+    const getData = await Promise.all(findClass.students.map(async (student) => {
       const studentInfo = await prisma.student.findUnique({
         where: {
           id: student.studentID
@@ -34,24 +32,44 @@ export const getAllGrades = async (req, res) => {
         select: {
           assignmentID: true,
           grade: true,
-          comments: true
         }
       })
+      // get assignment id, title, and grade from studentAssignment
       const getStudentAssignment = await Promise.all(studentHasAssignment.map(async (studentAssignment) => {
         const assignmentInfo = await prisma.assignment.findUnique({
           where: {
             id: studentAssignment.assignmentID
+          },
+          select: {
+            id: true,
+            title: true,
           }
         })
-        return {
-          assignmentInfo,
-          studentAssignment
+        const studentAssignmentGrade = {
+          assignmentID: assignmentInfo.id,
+          title: assignmentInfo.title,
+          grade: studentAssignment.grade,
         }
+        return studentAssignmentGrade
       }))
-      return {
-        studentInfo,
-        studentHasAssignment: getStudentAssignment
+      // get firstName and lastName from user table
+      const user = await prisma.user.findUnique({
+        where: {
+          id: studentInfo.userID
+        },
+        select: {
+          firstName: true,
+          lastName: true
+        }
+      })
+      const gradeInfo = {
+        studentID: studentInfo.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        assignmentInfo: getStudentAssignment,
+        finalGrade: student.finalGrade,
       }
+      return gradeInfo
     }))
     res.json(getData)
   } catch (error) {
